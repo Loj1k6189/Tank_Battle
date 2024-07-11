0b84c18c69f703c492624bd65af46cedc7ab8cbf
FOUNDSJJ <1219839548@qq.com>
2024-07-11 08:43:16 +0800

    Add files via upload

---
+++ Jump_End.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Jump_End : MonoBehaviour
+{
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Tank1" || collider.tag == "Tank2")
+        {
+            GameObject.Find("GradeBoard").SendMessage("TakeGrade");
+            SceneManager.LoadScene(2);
+        }
+    }
+}
